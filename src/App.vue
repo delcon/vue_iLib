21f6e6d2904a208d2007334fa70637b2a1e378e3
leo vetterli <leo.vetterli@gmail.com>
2019-10-23 22:26:14 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <iLibExample />
+  </div>
+</template>
+
+<script>
+import iLibExample from "./components/iLibExample";
+
+export default {
+  name: "App",
+  components: {
+    iLibExample 
+  }
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/iLibExample.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="wrapper">
+  <div class="no">{{formatted}}</div>
+</div>
+</template>
+
+<script>
+import NumFmt from "ilib/lib/NumFmt";
+const fmt = new NumFmt({
+   locale: "de-DE"
+});
+
+//const str = fmt.format(1234567.89);
+
+export default {
+  name: "iLibExample",
+  data: ()=>{
+    return {
+      testNo: 1234567.89
+    }
+  },
+  computed: {
+    formatted: function(){
+      return fmt.format(this.testNo);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.wrapper{
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+</style>
+
